c4a5136b7521e3ba290a014ec5bcd0964299ee01
Yobana <yjavier@miumg.edu.gt>
2022-10-15 06:42:42 -0600

    piloto

---
+++ app/Http/Controllers/PilotoController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Piloto;
+use Illuminate\Http\Request;
+
+class PilotoController extends Controller
+{
+    public function readAll()
+    {
+        return Piloto::all();
+    }
+
+    // metodos http crud
+    public function create(Request $request)
+    {
+        $data = $this->validateForm($request);
+
+        return Piloto::insert($data);
+    }
+
+    public function update($id, Request $request)
+    {
+        $data = $this->validateForm($request);
+
+        return Piloto::find($id)->update($data);
+    }
+    public function delete($id)
+    {
+        Piloto::find($id)->delete();
+    }
+    public function validateForm(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required',
+            'address' => 'required',
+        ]);
+        return $validatedData;
+    }
+}
